7b60ec8a2cced01d56514e50d26790b9b3e358f2
Marcus Almeida <marcusvinicius20.almeida@gmail.com>
2025-04-28 16:24:47 -0300

    feat: adicionada fonte poppins

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import React from 'react'
-import ReactDom from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-ReactDom.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter>
-      <App>
-      </App>
+      <App />
     </BrowserRouter>
-    </React.StrictMode>,
-)
+  </StrictMode>
+);
