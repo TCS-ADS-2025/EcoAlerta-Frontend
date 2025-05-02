ca370248879a84a61507d216a6dbca6099d87855
Marcus Almeida <marcusvinicius20.almeida@gmail.com>
2025-05-02 14:35:42 -0300

    feat: footer atualizado

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,29 @@
-import React from "react"
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import logo from "../../assets/logo-branca.png";
 
 const Footer = () => {
-    return(
-        <footer>
-            &copy;Eco Alerta 2025 | All rights reserved
-        </footer>
-    );
-}
+  return (
+    <footer>
+      <img className="logo" src={logo} alt="logo" />
+      <p>&copy;Eco Alerta 2025 | All rights reserved</p>
+      <div className="links">
+        <div className="contact">
+          <i className="bi bi-instagram"></i>
+          <a href="https://www.instagram.com/ecoalerta.cri/" target="_blank">
+            @ecoalerta.cri
+          </a>
+        </div>
+        <div className="contact">
+          <i className="bi bi-telephone"></i>
+          <a href="#">Prefeitura</a>
+        </div>
+        <div className="contact">
+          <i className="bi bi-telephone"></i>
+          <a href="https://api.whatsapp.com/send?phone=5548999010919">RACLI - Criciúma</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 export default Footer;
