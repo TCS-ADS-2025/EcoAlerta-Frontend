5ca3f94ac2899a27c377d5b0e20400239ce3f79d
Marcus Almeida <marcusvinicius20.almeida@gmail.com>
2025-06-09 12:51:00 -0300

    feat: role de usuários adicionada no Login

--- src/components/login-page/Login.tsx
+++ src/components/login-page/Login.tsx
@@ -9,7 +9,6 @@ import { LoginData } from "./../../interface/LoginData";
 import "./Login.css";
 import logo from "../../assets/logo-verde-sem-fundo.png";
 import Header from "../header/Header";
-import Footer from "../footer/Footer";
 
 const Login = (): ReactElement => {
   const navigate = useNavigate();
@@ -39,7 +38,9 @@ const Login = (): ReactElement => {
 
       if ([200, 201].includes(response.status)) {
         const token = response.data.token;
-        login(token);
+        const role = response.data.role;
+
+        login(token, role);
         alert(`Login realizado com sucesso!`);
         navigate("/");
       }
